6418c053a9534f3986a32da19afbe3f3a8aa9755
Cameron <cameronww7@users.noreply.github.com>
2019-04-07 13:56:08 -0700

    PyVar
    Python Variables Overview

---
+++ Python-Tutorials/Python-Variables.py
@@ -0,0 +1,50 @@
+# 3. Python Variables
+# Implicitly Declared, Strongly Typed
+
+print("Python Variables")
+
+pyVar = "Python Variables!"
+
+print(pyVar)
+
+userName = "Jack"
+lastName = "Brahh"
+
+print(userName)
+print(lastName)
+print(userName + lastName)
+
+a = "a"
+b = "b"
+c = "c"
+
+a = b = c
+
+print(a)
+
+varString = "This" \
+            "is" \
+            "a" \
+            "string"
+
+varInt = 55
+
+varLong = 32432
+
+varFloat = 3423.232
+
+varList = [1,2,3,4, "jack"]
+
+varTuple = (3,65,7,"Jim","Jacks")
+
+varDictionary = {'first': "First Item",
+                 'second': 'second Item'}
+
+print(varString)
+print(varInt)
+print(varLong)
+print(varFloat)
+print(varList)
+print(varTuple)
+print(varDictionary)
+
