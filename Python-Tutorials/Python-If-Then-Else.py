c67433dee33d17766b052073e9db45a590ea7e79
Cameron <cameronww7@users.noreply.github.com>
2019-04-08 19:44:58 -0700

    Py Else If Then
    Some ELse If Thenssss

---
+++ Python-Tutorials/Python-If-Then-Else.py
@@ -0,0 +1,15 @@
+# 5. Python If Then Else
+print("Python If Then Else")
+
+print(5 > 10)
+
+age = input("enter your Age: ")
+
+
+if int(age) > 20:
+    print("Your Older than 20")
+elif int(age) == 9:
+    print("Cats are cool")
+else:
+    print("Too Young")
+
