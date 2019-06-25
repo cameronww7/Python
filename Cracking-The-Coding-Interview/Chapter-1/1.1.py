0ff5bb3095935ce027aa89cd062c909d8f89a33a
Cameron <cameronww7@users.noreply.github.com>
2019-06-24 20:38:31 -0700

    Starting 1.1
    Setup 1.1

---
+++ Cracking-The-Coding-Interview/Chapter-1/1.1.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+# Prompt
+# 1.1 : Implement an algorithm to determine if a string has all unique characters.
+#       What if you cannot use additional data structures.
+print("1.1")
+
