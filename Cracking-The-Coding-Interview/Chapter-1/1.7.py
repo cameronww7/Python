69f605894b323bcb1959962e3c62db98e8567f80
Cameron <cameronww7@users.noreply.github.com>
2019-07-03 21:48:43 -0700

    1.7
    Finished, Roughhhh, was a tricky one!

--- Cracking-The-Coding-Interview/Chapter-1/1.7.py
+++ Cracking-The-Coding-Interview/Chapter-1/1.7.py
@@ -21,9 +21,35 @@ martrixSolution = [[7, 4, 1],
                    [8, 5, 2],
                    [9, 6, 3]]
 
+#print(len(matrix))
+#print("Goal Solution:\n", martrixSolution)
+
 def rotateMatrix90Deg(xMatrix):
-    print(xMatrix)
+    #print("", xMatrix)
 
+    dimension = len(matrix) - 1 # 0, 1, 2 so 3 dimensional
+    newMatrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
+    for row in range(len(xMatrix)):
+        for col in range(len(xMatrix[row])):
+            newMatrix[col][dimension] = xMatrix[row][col]
+        dimension = dimension - 1
 
-rotateMatrix90Deg(matrix)
+    return newMatrix
+
+print("Starting Program")
+print("0 rotate", matrix)
+
+matrix = rotateMatrix90Deg(matrix)
+print("1 rotate", matrix)
+
+matrix = rotateMatrix90Deg(matrix)
+print("2 rotate", matrix)
+
+matrix = rotateMatrix90Deg(matrix)
+print("3 rotate", matrix)
+
+matrix = rotateMatrix90Deg(matrix)
+print("4 rotate", matrix)
+
+print("End of Program")
