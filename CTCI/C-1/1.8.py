f46d99ad3faed157a9f69167c769358c38214bc2
Cameron <cameronww7@users.noreply.github.com>
2019-07-09 21:44:36 -0700

    Folder Change + First Take at 1.8
    Got something going, idk where its going, but attempting!

---
+++ CTCI/C-1/1.8.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+
+
+# Prompt 1.8 : Write an algorithm such that if an element in an MxN matrix is 0,
+#               its entire row and columns are set to 0
+
+
+print("1.8")
+
+
+matrix = [[1, 2, 3],
+          [4, 0, 6],
+          [7, 8, 0]]
+
+matrix2 = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+martrixSolution = [[7, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]]
+
+
+def zeroOutIfZeroFound(xMatrix):
+    #print("", xMatrix)
+
+    dimension = len(matrix)  # Messed up had - 1 here, that would make newMatrix too small
+    newMatrix = [[0 for row in range(dimension)] for col in range(dimension)]
+
+    dimension = len(matrix) - 1  # 0, 1, 2 so 3 dimensional
+    ifZeroFound = 0 #if a zero is found this be assigned where
+
+    print("1")
+    for row in range(len(xMatrix)):
+        for col in range(len(xMatrix[row])):
+            print("2 -", xMatrix[col][dimension])
+            if xMatrix[col][dimension] == 0:
+                print("3 -", xMatrix[col][dimension])
+                ifZeroFound = dimension
+                newMatrix[col][dimension] = 0
+            else:
+                print("4 -", ifZeroFound)
+                if ifZeroFound != 0:
+                    print("5")
+                    newMatrix[col][dimension] = 0
+                else:
+                    print("6")
+                    newMatrix[row][col] = xMatrix[row][col]
+        dimension = dimension - 1
+        ifZeroFound = 0
+
+
+    return newMatrix
+
+
+
+print("Starting Program")
+print("Start:", matrix2)
+
+matrix2 = zeroOutIfZeroFound(matrix2)
+
+print("  End:", matrix2)
+print("End of Program")
--- CTCI/Chapter-1/1.8.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-
-
-# Prompt 1.8 : Write an algorithm such that if an element in an MxN matrix is 0,
-#               its entire row and columns are set to 0
-
-
-print("1.8")
