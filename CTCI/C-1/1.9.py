b4e4cec11d01029421d6b47c7a67ee0522f19e82
Cameron <cameronww7@users.noreply.github.com>
2019-07-12 21:20:05 -0700

    1.9 - Start
    Setup and start of 1.9

---
+++ CTCI/C-1/1.9.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+# Prompt 1.9 : Assume you have a method isSubstring which checks if one word is a substring of another.
+# given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
+# isSubstring (e.g, "watterbottle" is a rotation of "erbottlewat")
+
+
+print("1.9")
+
+string1 = "watterbottle"
+string2 = "erbottlewat"
+solutionString = ""
+
+
+def IsSubstring(xString):
+    newString = ""
+
+
+    return newString
+
+
+print("Starting Program")
+
+print("string1:", string1)
+
+solutionString = IsSubstring(string1)
+
+print("solutionString:", solutionString)
+
+print("End of Program")
