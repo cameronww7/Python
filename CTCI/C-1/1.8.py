1bca39613cd5ecbc211896794b1d79bd65f587ad
Cameron <cameronww7@users.noreply.github.com>
2019-07-12 21:15:27 -0700

    1.8 - Cleaned up + Comments
    Added comments explaining what I did and cleaned up some Naming syntax.

--- CTCI/C-1/1.8.py
+++ CTCI/C-1/1.8.py
@@ -24,12 +24,22 @@ def zeroOutIfZeroFound(xMatrix):
     newMatrix = [["N" for row in range(dimension)] for col in range(dimension)]
 
     newMatrix = FindZeros(xMatrix, newMatrix)
+
+    PrintMatrix(newMatrix)
+
     newMatrix = SetZeros(xMatrix, newMatrix)
-    newMatrix = fillMatrixIn(xMatrix, newMatrix)
+
+    PrintMatrix(newMatrix)
+
+    newMatrix = FillMatrixIn(xMatrix, newMatrix)
 
     return newMatrix
 
-
+# FindZeros
+# Takes in 2 matrix's, first one is original and second is the newly created fresh one.
+# Slot by slot it checks each matrix slot for a Zeros and if one is found puts a 0 in the new matrix,
+# slots that no 0 is found it leaves the slot as the original set variable,
+# a N then passes back the new matrix with the all 0s found.
 def FindZeros(xMatrix1, xMatrix2):
     for row in range(len(xMatrix1)):
         for col in range(len(xMatrix1[row])):
@@ -37,6 +47,12 @@ def FindZeros(xMatrix1, xMatrix2):
                 xMatrix2[col][row] = 0
     return xMatrix2
 
+# SetZeros
+# Takes in 2 matrix's, first one is original and second is the newly created fresh one.
+# Then moves through each slot checking for a zero and if a zero is found the first for
+# loop will zero out the columns of the matrix that contains a zero. Then will go through
+# row by row finding the zeros and zeroing out the rows that there are found zeros in the
+# new matrix and returns that back.
 
 def SetZeros(xMatrix1, xMatrix2):
     ifZeroFound = 0
@@ -59,16 +75,22 @@ def SetZeros(xMatrix1, xMatrix2):
 
     return xMatrix2
 
-
-def fillMatrixIn(xMatrix1, xMatrix2):
+# FillMatrixIn
+# Takes in 2 matrix's, first one is original and second is the newly created fresh one.
+# The two for loops search for any variables in the matrix that contain a N and then
+# when a N is found will replace the N with its corresponding number in the ordinal matrix,
+# when finished all the slots that need to be Zeros are Zeros and the slots that don't
+# contain their original number and returns the new matrix.
+def FillMatrixIn(xMatrix1, xMatrix2):
     for row in range(len(xMatrix1)):
         for col in range(len(xMatrix1[row])):
             if xMatrix2[col][row] != 0:
                 xMatrix2[col][row] = xMatrix1[col][row]
     return xMatrix2
 
-
-def printMatrix(xMatrix):
+# PrintMatrix
+# Takes in a Matrix and then outputs the matrix nice and neat.
+def PrintMatrix(xMatrix):
     print("Printing Matrix")
     print("---------------")
 
@@ -83,9 +105,12 @@ def printMatrix(xMatrix):
 
 
 print("Starting Program")
-printMatrix(matrix)
+PrintMatrix(matrix)
 
 matrix3 = zeroOutIfZeroFound(matrix)
-printMatrix(matrix)
-printMatrix(matrix3)
+
+PrintMatrix(matrix)
+
+PrintMatrix(matrix3)
+
 print("\nEnd of Program")
