57672e2a2b5a06f273795682830f74a5f6847e59
Cameron <cameronww7@users.noreply.github.com>
2019-07-13 21:20:31 -0700

    1.6 - Cleaned up
    Went through 1.6 cleaning up and adding comments, and better output display. Removed debugging prints.

--- CTCI/C-1/1.6.py
+++ CTCI/C-1/1.6.py
@@ -10,39 +10,52 @@ from __future__ import print_function
 
 print("1.6")
 
-def strCompression(xInString):
+# StrCompression
+#   This function takes in a string, then parses through the string counting the number
+#   consecutive letters and building a string out with the letter and the number
+#   associated with the number of consecutive letters and returns the new build string.
+def StrCompression(xInString):
     xInString   = xInString.lower().replace(" ", "")
     newString   = ""
     letterCount = 1
 
+    # Moves through the passed in string counting the letters building the return string.
     for x in range(len(xInString)-1):
-        #print(xInString[x],xInString[(x+1)])
         if xInString[x] == xInString[(x+1)]:
             letterCount = letterCount + 1
-            #print("1Count= ",letterCount)
         else:
-            #print("2Count= ", letterCount)
             if letterCount > 1:
                 newString = newString + xInString[x] + letterCount.__str__()
             else:
                 newString = newString + xInString[x] + "1"
             letterCount = 1
 
+    # If Else Required to get the very last count to be added to the string.
     if letterCount > 1:
         newString = newString + xInString[x] + letterCount.__str__()
     else:
         newString = newString + xInString[len(xInString)-1] + "1"
 
-    #print("newString:", newString)
-
+    # If Else finally checking if the new string size is smaller than the original.
+    # If the new string is longer will return original, if shorter will return new string.
     if len(newString) < len(xInString):
-        print("newString:", newString)
         return newString
     else:
-        print("xInString:", xInString)
         return xInString
 
-strCompression("aabcccccaaa")
-strCompression("aabbccbbbbbbba")
-strCompression("aabbcca")
-strCompression("AAbbCCZZZZZ")
+
+print("\nStarting Program")
+
+solutionStr1 = StrCompression("aabcccccaaa")
+print("\naabcccccaaa =", solutionStr1)
+
+solutionStr2 = StrCompression("aabbccbbbbbbba")
+print("\naabbccbbbbbbba =", solutionStr2)
+
+solutionStr3 = StrCompression("aabbcca")
+print("\naabbcca =", solutionStr3)
+
+solutionStr4 = StrCompression("AAbbCCZZZZZ")
+print("\nAAbbCCZZZZZ =", solutionStr4)
+
+print("\nEnd of Program")
