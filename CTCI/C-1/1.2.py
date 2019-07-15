7792bb7611036649ff6a20ee693d06651168e4ef
Cameron <cameronww7@users.noreply.github.com>
2019-07-14 21:16:47 -0700

    1.2 - Cleaned up
    Cleaned up and broke out functions, added comments and fixed up syntax!

--- CTCI/C-1/1.2.py
+++ CTCI/C-1/1.2.py
@@ -6,25 +6,61 @@ from __future__ import print_function
 #   "sumit" and "tiums" are permutations of each other.
 #   "abcd" and bdea" are not permutations of each other.
 
+
+# IsStringPermutation
+#   This function takes in two strings then moves through each slot
+#   in string 2 comparing if they have the same letters and if they
+#   are the same size with the same variables they are the
+#   permutation of each other.
+def IsStringPermutation(xString1, xString2):
+    sameLetterCount = 0
+
+    if IsSameSize(xString1, xString2):
+        for x in range(len(string1)):
+            for i in range(len(string2)):
+                if string1[x] == string2[i]:
+                    sameLetterCount = sameLetterCount + 1
+        if sameLetterCount == len(string1) and sameLetterCount == len(string2):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+# IsSameSize
+#   This function takes two strings in and returns if they are the
+#   same size or not with a true or false return
+def IsSameSize(xString1, xString2):
+    if len(xString1) == len(xString2):
+        return True
+    else:
+        return False
+
+
+# PrintResult
+#   This function takes in a true or false and returns the
+#   correct output message depending on what was passed in.
+def PrintResult(xResult):
+    if xResult:
+        print("\nTwo Strings are permutations")
+    else:
+        print("\nStrings are not permutations")
+
 print("1.2")
+
+print("\nStarting Program\n")
 
 string1 = "sumit"
 string2 = "tiums"
 
-#string1 = "abcd"
-#string2 = "bdea"
+print("String 1:", string1)
+print("String 2:", string2)
 
-t = 0
+result = IsStringPermutation(string1, string2)
 
-for x in range(len(string1)):
-    for i in range(len(string2)):
-        if (string1[x] == string2[i]):
-            t = t + 1
+PrintResult(result)
 
+print("\nThe Big O of this Solution is O(n^2)")
 
-if (t == len(string1) and t == len(string2)):
-    print("Two Strings are permutations")
-else:
-    print("Strings are not permutations")
-
-print(" The Big O of this Solution is O(n^2)")
+print("\nEnd of Program")
