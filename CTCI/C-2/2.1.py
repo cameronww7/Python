5f225c855078ee479aff5815ed4271559e63a6f0
Cameron <cameronww7@users.noreply.github.com>
2019-07-16 21:04:25 -0700

    2.1 - Setup
    Setup of 2.1

---
+++ CTCI/C-2/2.1.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+"""
+ Prompt 2.1 : Remove Dups
+               Write code to remove duplicates from an unsorted linked list.
+        Follow up - Ho would you solve this problem if a temporary buffer is not allowed.
+"""
+
+print("2.1")
+
+
+
+
+
+print("Starting Program")
+
+
+
+
+print("\nEnd of Program")
