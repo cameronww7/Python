517f349f27893904a7804ce749d5445dfc417f06
Cameron <cameronww7@users.noreply.github.com>
2019-07-18 21:23:45 -0700

    2.1 - Cleaned Up
    Added some comments, cleaned up syntax, removed debug code.

--- CTCI/C-2/2.1.py
+++ CTCI/C-2/2.1.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 """
- Prompt 2.1 : Remove Dups
+ Prompt 2.1 : Python - Remove Dups
                Write code to remove duplicates from an unsorted linked list.
         Follow up - Ho would you solve this problem if a temporary buffer is not allowed.
         
@@ -22,19 +22,23 @@ class Node:
         self.data = data_value
         self.next_node = next_node
 
-    # Getter - Sends the pointer to the next node
+    # get_next - Getter
+    #   Sends the pointer to the next node
     def get_next(self):
         return self.next_node
 
-    # Setter - Sets the node current node to point at the next node passed in
+    # set_next_node - Setter
+    #   Sets the node current node to point at the next node passed in
     def set_next_node(self, new_node):
         self.next_node = new_node
 
-    # Getter - Sends the data of the current Node
+    # get_data - Getter
+    #   Sends the data of the current Node
     def get_data(self):
         return self.data
 
-    # Setter - Sets the passed in data to the current Node
+    # set_data - Setter
+    #   Sets the passed in data to the current Node
     def set_data(self, new_data):
         self.data = new_data
 
@@ -50,49 +54,53 @@ class LinkedList (object):
         self.root = root_node
         self.size = 0
 
-    # Getter - Returns the Current Size of the LinkedList
+    # get_size - Getter
+    #   Returns the Current Size of the LinkedList
     def get_size(self):
         return self.size
 
-    # Add - Adds a new node to the LinkedList, adds to the End
+    # add_new_node - Add
+    #   Adds a new node to the LinkedList, adds to the End
     def add_new_node(self, new_data):
         print("Adding :", new_data)
 
-        new_node = Node(new_data, self.root)
-        self.root = new_node
+        new_node = Node(new_data, self.root)  # Create New Node
+        self.root = new_node # adds a New Node in Line
         self.size += 1
 
-    # Remove - Removes the passed in node number from the LinkedList
+    # remove_node - Remove
+    #   Removes the passed in node number from the LinkedList
     def remove_node(self, node_to_remove):
         print("Removing :", node_to_remove)
 
-        current_node = self.root
-        previous_node = None
+        current_node = self.root  # Grabs Root Node
+        previous_node = None  # Previous Node is Created set to null
 
-        if current_node is not None:
-            if current_node.get_data() == node_to_remove:
-                self.root = current_node.get_next()
-                current_node = None
+        if current_node is not None:  # checks root node is not null
+            if current_node.get_data() == node_to_remove:  # checks if root node is node to remove
+                self.root = current_node.get_next()  # set root node to next node
+                current_node = None  # clears old root node
                 print(node_to_remove, "Was Found & Removed")
                 return True
 
-        while current_node:
-            if current_node.get_data() == node_to_remove:
+        while current_node:   # Loop Though list
+            if current_node.get_data() == node_to_remove:  # checks if current node is node to remove
                 if previous_node:
-                    previous_node.set_next_node(current_node.get_next())
+                    previous_node.set_next_node(current_node.get_next()) # sets previous node to next node
                 else:
-                    self.root = current_node
+                    self.root = current_node  # sets root node to current node
                 self.size -= 1
                 print(node_to_remove, "Was Found & Removed")
                 return True  # data is removed
             else:
-                previous_node = current_node
-                current_node = current_node.get_next()
+                previous_node = current_node  # Increments previous node
+                current_node = current_node.get_next()   # Increments current node
 
-        print(node_to_remove, "Was Not Found & Removed")
+        print(node_to_remove, "Does not Exist")
         return False  # data was not found/removed
 
-    # Find - Finds the passed in node and returns its Data
+    # find_node
+    #   Finds the passed in node and returns its Data
     def find_node(self, data_to_find):
         print("Finding: ", data_to_find)
 
@@ -106,7 +114,8 @@ class LinkedList (object):
 
         return None
 
-    # Print - Prints all the Nodes in the list, Start to End
+    # print_list
+    #   Prints all the Nodes in the list, Start to End
     def print_list(self):
         print("\nPrinting LinkedList Nodes")
         print("--------------------------")
@@ -121,21 +130,20 @@ class LinkedList (object):
 
         print("--------------------------\n")
 
-    # Core Function to this Project, Removes Duplicates
+    # Removes Duplicates
+    #   Searches LinkedList and Removes all Duplicate Nodes
     def remove_duplicates(self):
         print("Removing all Duplicates")
         current_node = self.root
 
-        while current_node:
-            #print("1 - ", current_node.get_data())
-            dup_search = current_node.get_next()
-            while dup_search:
-                #print("2 - ", dup_search.get_data())
+        while current_node:  # Loops through list
+            dup_search = current_node.get_next()  # increments dup_search to 1 node ahead
+            while dup_search: # Loops though dup_search
                 if current_node.get_data() == dup_search.get_data():
-                    self.remove_node(current_node.get_data())
-                dup_search = dup_search.get_next()
-            current_node = current_node.get_next()
-        #print("3")
+                    self.remove_node(current_node.get_data())  # Removes dup Node
+                dup_search = dup_search.get_next()  # increments dup_search to 1 node ahead
+            current_node = current_node.get_next()  # increments current_node to 1 node ahead
+
 
 print("\nStarting Program\n")
 
