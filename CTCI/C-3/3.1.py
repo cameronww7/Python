893da4535397ab839d09cbb1eb28bce8680643e2
Cameron <cameronww7@users.noreply.github.com>
2019-07-31 21:03:43 -0700

    New Folder + 3.1 File Creation

---
+++ CTCI/C-3/3.1.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+"""
+ Prompt 3.1 : Python - 
+                        
+
+EX:       
+        Input:  
+        Output: 
+
+"""
+
+print("3.1")
+
+
+
+print("\nStarting Program\n")
+
+
+
+print("\nEnd of Program")
+
