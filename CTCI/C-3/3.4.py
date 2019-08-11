afcbc74881d57cccba0b69959b57ca1d9e858a7b
Cameron <cameronww7@users.noreply.github.com>
2019-08-10 20:41:56 -0700

    3.4 - Start

---
+++ CTCI/C-3/3.4.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+
+"""
+ Prompt 3.4 : Python - Implement a MyQueue Class which implements a queue using two stacks.
+
+                    First-in-First out
+
+            The uniqueness of queue lies in the way items are added and removed. 
+            The items are allowed at on end but removed form the other end. 
+EX:       
+        Input:  
+        Output: 
+
+"""
+
+print("3.4")
+
+
+class MyQueue:
+    def __init__(self):
+        self.queue = list()
+
+    # add_to_queue
+    #   Pushes on a new node
+    def add_to_queue(self, new_node):
+        # Insert method to add element
+        if new_node not in self.queue:
+            self.queue.insert(0, new_node)
+            return True
+        return False
+
+    # remove_from_queue
+    #    Pop method to remove element
+    def remove_from_queue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop()
+        return ("No elements in Queue!")
+
+
+print("\nStarting Program\n")
+
+my_queue = MyQueue()
+
+print("\nEnd of Program")
+
