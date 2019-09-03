dbcfd997a72718adeb7250e7533b60d6f4f866fc
Cameron <cameronww7@users.noreply.github.com>
2019-09-02 18:13:25 -0700

    Numbers!

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/11-Py-Numbers.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+"""
+ Prompt 11 Numbers :
+
+
+"""
+
+print("11 Numbers")
+
+print(2 + 1)
+
+print(2 - 1)
+
+print(7/4)
+
+print(7%4)
+
+# Two the Power of 3
+print(2**3)
+
+temp = 2 + 2
+
+
+temp = 2 + temp
+
+print(temp)
+
+# https://docs.python.org/2/tutorial/floatingpoint.html
+print(0.1 + 0.2 - 0.3)
