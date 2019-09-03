668a65a6c4093b0d7cc617101a601b448c38051b
Cameron <cameronww7@users.noreply.github.com>
2019-09-02 18:20:54 -0700

    Variables

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/13-Py-Variables.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+"""
+ Prompt 13 Variables :
+"""
+
+print("13 Variables")
+
+my_dog = 2
+print("my_dog " + my_dog.__str__())
+
+my_dog = ['Joe', 'Maria']
+print("my_dog " + my_dog.__str__())
+
+print("Checking Type", type(my_dog))
+
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+
+print(my_taxes)
