bd6727bf9fabc3d67417cbd9d977cd811a0a8006
Cameron <cameronww7@users.noreply.github.com>
2019-09-21 21:37:54 -0700

    Finish Dictionaries

--- Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/22-Py-Dictionaries.py
+++ Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/22-Py-Dictionaries.py
@@ -5,3 +5,37 @@ from __future__ import print_function
 """
 
 print("22 Dictionaries")
+
+# Dictionaries: Objects retrieved by key name.
+#   Unordered and can not be sorted
+
+# Lists: Objects retrieved by location
+#   Ordered Sequence can be indexed or sliced
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+
+my_price = {'apple': 2.99, 'oranges': 1.99, 'milk': 5.99}
+
+print(my_price['apple'])
+
+my_dict = {'key1': 123, 'key2': [12, 23, 33], 'key3': ['item0', 'item1', 'item2']}
+
+# Let's call items from the dictionary
+print(my_dict['key3'])
+
+print(my_dict['key3'][0])
+
+# Can then even call methods on that value
+print(my_dict['key3'][0].upper())
+
+# Create a new dictionary
+d = {}
+
+# Create a new key through assignment
+d['animal'] = 'Dog'
+
+# Can do this with any object
+d['answer'] = 42
+
+print(d)
