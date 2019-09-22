53082a1b3d3eacfa51478d9d057623e3fa32b2f9
Cameron <cameronww7@users.noreply.github.com>
2019-09-21 21:51:28 -0700

    Sets

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/25-Py-Sets.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+"""
+ Prompt 25 sets :
+"""
+
+print("25 sets")
+
+"""
+Sets
+Sets are an unordered collection of unique elements. We 
+can construct them by using the set() function. Let's 
+go ahead and make a set to see how it works
+"""
+
+
+x = set()
+
+
+# We add to sets with the add() method
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(5)
+
+print(x)
+
+# Create a list with repeats
+list1 = [1, 1, 2, 2, 3, 4, 5, 6, 1, 1]
+
+print(list1)
