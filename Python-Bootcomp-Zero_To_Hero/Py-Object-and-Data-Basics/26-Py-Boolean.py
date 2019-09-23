2dbc8350da6cc8f5575020ca4c7fcfdd0085469b
Cameron <cameronww7@users.noreply.github.com>
2019-09-22 21:30:08 -0700

    Booleans

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Object-and-Data-Basics/26-Py-Boolean.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+"""
+ Prompt 26 Booleans :
+"""
+
+print("26 Booleans")
+
+"""
+Booleans
+
+Python comes with Booleans (with predefined True and False displays 
+that are basically just the integers 1 and 0). It also has a 
+placeholder object called None. Let's walk through a few quick 
+examples of Booleans (we will dive deeper into them later in this course).
+"""
+
+# Set object to be a boolean
+a = True
+
+
+#Show
+print(a)
+
+# Output is boolean
+print(1 > 2)
+
+
+# None placeholder
+b = None
+
+# Show
+print(b)
