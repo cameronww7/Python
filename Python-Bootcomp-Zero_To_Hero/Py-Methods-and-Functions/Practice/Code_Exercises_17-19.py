5f214c5390e5c1f9563c4734b611545e3daee5a4
Cameron <cameronww7@users.noreply.github.com>
2020-04-01 20:09:55 -0700

    Starting Ex 8

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/Practice/Code_Exercises_17-19.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+
+"""
+ Prompt Code_Exercise_17-19.py
+"""
+
+print("Code_Exercise_17-19.py")
+
+"""
+Purpose:
+
+The purpose of this file is to go through the coding exercise and knock them out one by one!
+
+So, here we go!
+"""
