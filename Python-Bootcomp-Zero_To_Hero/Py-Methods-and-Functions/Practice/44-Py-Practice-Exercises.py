1aed11583b3f40254b73953cd0c422eda0c716ac
Cameron <cameronww7@users.noreply.github.com>
2020-04-09 19:14:05 -0700

    Finished Problem 13 Last one

--- Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/Practice/44-Py-Practice-Exercises.py
+++ Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/Practice/44-Py-Practice-Exercises.py
@@ -319,10 +319,13 @@ def pr13(xVar):
     # mapping the alphabet to specific 5-line combinations of patterns.
     # <br>For purposes of this exercise, it's ok if your dictionary stops at \"E\"."
     print("\nProblem 13")
+    patterns = {1: '  *  ', 2: ' * * ', 3: '*   *', 4: '*****', 5: '**** ', 6: '   * ', 7: ' *   ', 8: '*   * ',
+                9: '*    '}
+    alphabet = {'A': [1, 2, 4, 3, 3], 'B': [5, 3, 5, 3, 5], 'C': [4, 9, 9, 9, 4], 'D': [5, 3, 3, 3, 5],
+                'E': [4, 9, 4, 9, 4]}
+    for pattern in alphabet[xVar.upper()]:
+        print(patterns[pattern])
 
-    xVar = xVar
 
-
-
-print(pr13("temp"))
+print(pr13("a"))
 
