1d60fc8e3c4e8c87592738cf7f60f49bc1138687
Cameron <cameronww7@users.noreply.github.com>
2020-04-10 20:56:46 -0700

    Filter function start

--- Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/49-Lambda-Exp-Map-Filter-Func.py
+++ Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/49-Lambda-Exp-Map-Filter-Func.py
@@ -47,4 +47,12 @@ name = ["joe", "amea", "noface"]
 print(list(map(splicer, name)))
 
 
+"""
+filter function
+The filter function returns an iterator yielding those items of iterable 
+for which function(item) is true. Meaning you need to filter by a function 
+that returns either True or False. Then passing that into filter (along 
+with your iterable) and you will get back only the results that would 
+return True when passed to the function.
+"""
 
