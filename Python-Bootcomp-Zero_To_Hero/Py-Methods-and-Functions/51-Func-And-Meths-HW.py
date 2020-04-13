884a5f78b8543b52ddc0d30c3babe4644d3e7d55
Cameron <cameronww7@users.noreply.github.com>
2020-04-12 21:14:20 -0700

    Start of HW problems

---
+++ Python-Bootcomp-Zero_To_Hero/Py-Methods-and-Functions/51-Func-And-Meths-HW.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+"""
+ Prompt 51-Func-And-Meths-HW
+"""
+
+print("51-Func-And-Meths-HW")
+
+"""
+Nested Statements and Scope
+Now that we have gone over writing our own functions, it's important 
+to understand how Python deals with the variable names you assign. 
+When you create a variable name in Python the name is stored in a 
+name-space. Variable names also have a scope, the scope determines 
+the visibility of that variable name to other parts of your code.
+"""
+
+
+
+def pr1(xVal):
+    # Write a function that computes the volume of a sphere given its radius.
+    # 4/3 * xVal ^3
+    print("\nProblem 1")
+
+
+print(pr1(2))
