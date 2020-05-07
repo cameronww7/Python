abd676d8b19a9d3d353eb72c4c6fb9af75acb633
Cameron <cameronww7@users.noreply.github.com>
2020-05-06 19:40:12 -0700

    Starting OOP

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/60-OOP-Attributes-and-class.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+"""
+ Prompt 60-OOP-Attributes-and-class
+"""
+
+print("60-OOP-Attributes-and-class")
+
+"""
+Object Oriented Programming
+Object Oriented Programming (OOP) tends to be one of the major obstacles 
+for beginners when they are first starting to learn Python.
+
+There are many, many tutorials and lessons covering OOP so feel free to 
+Google search other lessons, and I have also put some links to other 
+useful tutorials online at the bottom of this Notebook.
+
+For this lesson we will construct our knowledge of OOP in Python by 
+building on the following topics:
+
+Objects
+Using the class keyword
+Creating class attributes
+Creating methods in a class
+Learning about Inheritance
+Learning about Polymorphism
+Learning about Special Methods for classes
+
+"""
+
+print("\n60 - OOP - Attributes and Classes\n")
