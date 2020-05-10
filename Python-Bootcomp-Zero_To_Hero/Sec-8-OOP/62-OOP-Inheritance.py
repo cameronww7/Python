ba68e7cf21bfaa2315b7c2d8da9e072625d8fcc3
cameronww7 <cameronww7@gmail.com>
2020-05-09 20:59:49 -0700

    Finished Inheritance

--- Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/62-OOP-Inheritance.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/62-OOP-Inheritance.py
@@ -13,21 +13,21 @@ The newly formed classes are called derived classes, the classes that we derive 
 are called base classes. Important benefits of inheritance are code reuse and reduction 
 of complexity of a program. The derived classes (descendants) override or extend the 
 functionality of base classes (ancestors).
-
 """
 
 print("\n62-OOP-Inheritance\n")
 print("- - - - - - - - - - ")
 
-#Let's see an example by incorporating our previous work on the Dog class:
+# Let's see an example by incorporating our previous work on the Dog class:
 
 
 class Animal:
+    # this is our Base class and other classes will inheritant this class
     def __init__(self):
         print("Animal created")
 
     def whoAmI(self):
-        print("Animal")
+        print("I am a Animal")
 
     def eat(self):
         print("Eating")
@@ -39,7 +39,21 @@ class Dog(Animal):
         print("Dog created")
 
     def whoAmI(self):
-        print("Dog")
+        print("I am a Dog")
+
+    def eat(self):
+        print("I am a Dog Eating")
 
     def bark(self):
         print("Woof!")
+
+
+print("\nCreating Animal")
+myanimal = Animal()
+myanimal.whoAmI()
+
+print("\nCreating Dog Class")
+mydog = Dog()
+mydog.whoAmI()
+mydog.eat()
+mydog.bark()
