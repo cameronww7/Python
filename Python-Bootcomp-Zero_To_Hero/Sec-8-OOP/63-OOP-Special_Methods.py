6324e3868a00727fc4fc4543512e9f8ec46189e5
cameronww7 <cameronww7@gmail.com>
2020-05-11 20:16:50 -0700

    Finished Special Methods

--- Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/63-OOP-Special_Methods.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/63-OOP-Special_Methods.py
@@ -8,13 +8,42 @@ from __future__ import print_function
 print("63-OOP-Special_Methods")
 
 """
-We've learned that while functions can take in different arguments, 
-methods belong to the objects they act on. In Python, polymorphism 
-refers to the way in which different object classes can share the 
-same method name, and those methods can be called from the same 
-place even though a variety of different objects might be passed 
-in. The best way to explain this is by example:
+
+Special Methods
+Finally let's go over special methods. Classes in Python can implement 
+certain operations with special method names. These methods are not 
+actually called directly but by Python specific language syntax. 
 """
 
 print("\n63-OOP-Special_Methods\n")
 print("- - - - - - - - - - ")
+
+
+class Book:
+    def __init__(self, title, author, pages):
+        print("A book is created")
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "Title: %s, author: %s, pages: %s" %(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book is destroyed")
+
+
+book = Book("Python Rocks!", "Jose Portilla", 159)
+
+# Special Methods
+print("\nPrint Book")
+print(book)
+
+print("\nLength of Book")
+print(len(book))
+
+print("\nDeleting Book")
+del book
