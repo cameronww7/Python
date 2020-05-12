d2b23f6440227ebc3fa2437674c84101be057828
cameronww7 <cameronww7@gmail.com>
2020-05-11 20:19:15 -0700

    Start of OOP HW

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/64-OOP-Homework.py
@@ -0,0 +1,73 @@
+
+from __future__ import print_function
+
+"""
+ Prompt 64-OOP-Homework
+"""
+
+print("64-OOP-Homework")
+
+"""
+
+
+"""
+
+print("\n64-OOP-Homework\n")
+print("- - - - - - - - - - ")
+
+
+"""
+Problem 1
+Fill in the Line class methods to accept coordinates as a pair 
+of tuples and return the slope and distance of the line.
+"""
+
+
+class Line:
+
+    def __init__(self, coor1, coor2):
+        pass
+
+    def distance(self):
+        pass
+
+    def slope(self):
+        pass
+
+
+# EXAMPLE OUTPUT
+
+coordinate1 = (3, 2)
+coordinate2 = (8, 10)
+
+li = Line(coordinate1, coordinate2)
+
+li.distance()
+
+li.slope()
+
+"""
+Problem 2
+Fill in the class
+"""
+
+
+class Cylinder:
+
+    def __init__(self, height=1, radius=1):
+        pass
+
+    def volume(self):
+        pass
+
+    def surface_area(self):
+        pass
+
+
+# EXAMPLE OUTPUT
+
+c = Cylinder(2, 3)
+
+c.volume()
+
+c.surface_area()
