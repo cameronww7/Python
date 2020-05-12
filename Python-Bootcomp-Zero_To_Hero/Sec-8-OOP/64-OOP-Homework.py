ba1f5419e26cd608d93b6bb3755fc112f411e7ad
cameronww7 <cameronww7@gmail.com>
2020-05-11 20:27:23 -0700

    Finished setup of problems

--- Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/64-OOP-Homework.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/64-OOP-Homework.py
@@ -25,13 +25,18 @@ of tuples and return the slope and distance of the line.
 
 class Line:
 
-    def __init__(self, coor1, coor2):
-        pass
+    def __init__(self, xCoor1, xCoor2):
+        self.corr1 = xCoor1
+        self.corr2 = xCoor2
 
     def distance(self):
+        # Get Distance Formula
+        # https://www.chilimath.com/lessons/intermediate-algebra/distance-formula/
         pass
 
     def slope(self):
+        # Need Slope Formula
+        # https://calcworkshop.com/graphing-linear-equations/slope-formula/?PageSpeed=noscript
         pass
 
 
@@ -40,10 +45,13 @@ class Line:
 coordinate1 = (3, 2)
 coordinate2 = (8, 10)
 
+print("\nCreating Line Object")
 li = Line(coordinate1, coordinate2)
 
+print("\nGetting Distance of Line Object")
 li.distance()
 
+print("\nGetting Slope of Line Object")
 li.slope()
 
 """
@@ -54,20 +62,28 @@ Fill in the class
 
 class Cylinder:
 
-    def __init__(self, height=1, radius=1):
-        pass
+    def __init__(self, xHeight = 1, xRadius = 1):
+        self.height = xHeight
+        self.radius = xRadius
 
     def volume(self):
+        # Get Volume of a Cylinder
+        # https://www.mathopenref.com/cylindervolume.html
         pass
 
     def surface_area(self):
+        # Get the Surface Area of a Cylinder
+        # https://www.w3resource.com/python-exercises/math/python-math-exercise-5.php
         pass
 
 
 # EXAMPLE OUTPUT
 
+print("\nCreating Cylinder Object")
 c = Cylinder(2, 3)
 
+print("\nGetting Volume of Cylinder Object")
 c.volume()
 
+print("\nGetting Surface Area of Cylinder Object")
 c.surface_area()
