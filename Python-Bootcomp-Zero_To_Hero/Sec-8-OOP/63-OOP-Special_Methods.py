22a1db68dc78750038b72342e26948a306bb4793
cameronww7 <cameronww7@gmail.com>
2020-05-11 20:13:44 -0700

    StartinG Special Methods

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-8-OOP/63-OOP-Special_Methods.py
@@ -0,0 +1,20 @@
+
+from __future__ import print_function
+
+"""
+ Prompt 63-OOP-Special_Methods
+"""
+
+print("63-OOP-Special_Methods")
+
+"""
+We've learned that while functions can take in different arguments, 
+methods belong to the objects they act on. In Python, polymorphism 
+refers to the way in which different object classes can share the 
+same method name, and those methods can be called from the same 
+place even though a variety of different objects might be passed 
+in. The best way to explain this is by example:
+"""
+
+print("\n63-OOP-Special_Methods\n")
+print("- - - - - - - - - - ")
