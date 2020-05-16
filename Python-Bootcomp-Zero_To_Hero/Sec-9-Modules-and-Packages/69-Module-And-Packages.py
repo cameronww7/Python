0bd12ec532b0eb38e583808d4934917ecb32e39e
cameronww7 <cameronww7@gmail.com>
2020-05-15 20:13:07 -0700

    69-Module-And-Packages

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-9-Modules-and-Packages/69-Module-And-Packages.py
@@ -0,0 +1,18 @@
+
+from __future__ import print_function
+from mymodule import my_func
+
+"""
+ Prompt 69-Module-And-Packages
+"""
+
+print("69-Module-And-Packages")
+
+"""
+
+"""
+
+print("\n69-Module-And-Packages\n")
+print("- - - - - - - - - - ")
+
+my_func()
