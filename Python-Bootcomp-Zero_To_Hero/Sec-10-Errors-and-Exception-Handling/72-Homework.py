3be0c6c58d51d49e4de0d96a8eff2b787fbe9491
cameronww7 <cameronww7@gmail.com>
2020-05-27 20:10:42 -0700

    Working in wrong file

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+"""
+ Prompt 72-Errors-and-Exception-Handling - Homework
+"""
+
+print("72-Errors-and-Exception-Handling - Homework")
+
+"""
+
+"""
+
+print("\n72-Errors-and-Exception-Handling - Homework\n")
+print("- - - - - - - - - - ")
+
+
+"""
+Problem 1
+Handle the exception thrown by the code below by using try and except blocks.
+"""
+try:
+    for i in ['a' ,'b', 'c']:
+        print(i**2)
+except:
+    print("Error")
