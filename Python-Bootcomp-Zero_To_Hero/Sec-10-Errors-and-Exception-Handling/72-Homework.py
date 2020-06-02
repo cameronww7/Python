e02a8a1e429b572792def868e04e5b731caafa20
cameronww7 <cameronww7@gmail.com>
2020-06-01 19:47:09 -0700

    Finished Problem 2

--- Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
@@ -21,5 +21,27 @@ Handle the exception thrown by the code below by using try and except blocks.
 try:
     for i in ['a' ,'b', 'c']:
         print(i**2)
+except TypeError:
+    print("TypeError")
+
 except:
     print("Error")
+
+
+"""
+Problem 2
+Handle the exception thrown by the code below by using try and except blocks. 
+Then use a finally block to print 'All Done.
+"""
+x = 5
+y = 0
+
+try:
+    z = x/y
+
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+
+finally:
+    print("All Done")
+
