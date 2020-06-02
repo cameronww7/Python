c65b3bf49d71b46aa9cb5d8e48f7eb2787483516
cameronww7 <cameronww7@gmail.com>
2020-06-01 19:51:21 -0700

    Finished Problem 3

--- Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
@@ -45,3 +45,29 @@ except ZeroDivisionError:
 finally:
     print("All Done")
 
+
+"""
+Problem 3
+Write a function that asks for an integer and prints the square of it. 
+Use a while loop with a try, except, else block to account for incorrect inputs.
+"""
+def ask():
+    while True:
+        try:
+            # First Attempt - will be successful if an Int comes in
+            val = int(input("Please enter an integer: "))
+        except ValueError:
+            # If an error pops it will display an Error and re-prompted the user for an Int
+            print("ValueError -- Looks like you did not enter an integer!")
+            continue
+        except:
+            # If an error pops it will display an Error and re-prompted the user for an Int
+            print("An Error Occurred")
+            continue
+        else:
+            # Breaks the infinite while loop if a int is entered
+            print("Your number squared is: {}".format(val**2))
+            break
+
+
+ask()
