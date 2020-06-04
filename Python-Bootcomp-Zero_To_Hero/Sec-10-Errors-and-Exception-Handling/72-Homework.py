19db400a9cf0793cab1b4ba38467ade17be51bcc
cameronww7 <cameronww7@gmail.com>
2020-06-03 20:21:15 -0700

    Installed pyLint

--- Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-10-Errors-and-Exception-Handling/72-Homework.py
@@ -19,7 +19,7 @@ Problem 1
 Handle the exception thrown by the code below by using try and except blocks.
 """
 try:
-    for i in ['a' ,'b', 'c']:
+    for i in ['a', 'b', 'c']:
         print(i**2)
 except TypeError:
     print("TypeError")
