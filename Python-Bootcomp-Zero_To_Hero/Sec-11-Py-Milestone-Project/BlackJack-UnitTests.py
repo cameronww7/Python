f2ca9d384edb55737ccdd9f8c006de32e56ec33f
cameronww7 <cameronww7@gmail.com>
2020-06-12 20:36:04 -0700

    Renamed Unit Test 2

--- Python-Bootcomp-Zero_To_Hero/Sec-11-Py-Milestone-Project/BlackJack-UnitTests.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-11-Py-Milestone-Project/BlackJack-UnitTests.py
@@ -31,6 +31,9 @@ class TestBlackJackGame(unittest.TestCase, Deck):
         self.assertEqual(result, 'MaKe Sure Unit Tests are WORKING')
 
     def test_two_word(self):
+        pass
+
+    def test_three_word(self):
         suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
         ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
         values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
