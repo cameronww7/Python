42ea4f53ad5c421c2a162c46387dd46b6c501e1c
cameronww7 <cameronww7@gmail.com>
2020-06-18 19:52:44 -0700

    Added lose bet

--- Python-Bootcomp-Zero_To_Hero/Sec-11-Py-Milestone-Project/BlackJackGame.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-11-Py-Milestone-Project/BlackJackGame.py
@@ -198,8 +198,8 @@ class Chips:
         return self.total
 
     def lose_bet(self):
-        pass
-
+        self.total = self.total - self.bet
+        return self.total
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
