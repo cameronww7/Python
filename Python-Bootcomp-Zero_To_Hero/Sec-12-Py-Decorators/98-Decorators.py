279399f8f0e1c399f3a5221eb8f56505f9052331
cameronww7 <cameronww7@gmail.com>
2020-08-10 19:56:50 -0700

    Starting 98-Decorators

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-12-Py-Decorators/98-Decorators.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+"""
+ Prompt 98-Decorators
+"""
+
+print("98-Decorators")
+
+"""
+
+Decorators
+Decorators can be thought of as functions which modify the functionality of another function. They help to make your code shorter and more "Pythonic".
+
+To properly explain decorators we will slowly build up from functions. Make sure to run every cell in this Notebook for this lecture to look the same on your own computer.
+
+So let's break down the steps:
+"""
+
+print("\n98-Decorators\n")
+print("- - - - - - - - - - ")
+
+
