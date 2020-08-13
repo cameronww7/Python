6c92a6170c52a05e764ad7b06b2e1d12619fa9fc
cameronww7 <cameronww7@gmail.com>
2020-08-12 20:18:44 -0700

    Generator Prompts filled out

--- Python-Bootcomp-Zero_To_Hero/Sec-13-Py-Generators/101-Generators-HW.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-13-Py-Generators/101-Generators-HW.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import random
 
 """
  Prompt 101-Generators-HW
@@ -11,3 +12,57 @@ print("101-Generators-HW")
 """
 
 print("\n101-Generators-HW")
+
+
+"""
+Problem 1
+Create a generator that generates the squares of numbers up to some number N.
+"""
+
+
+def gensquares(N):
+
+    pass
+
+for x in gensquares(10):
+    print(x)
+
+
+"""
+Problem 2
+Create a generator that yields "n" random numbers between a low and high number 
+(that are inputs).
+Note: Use the random library. For example:
+"""
+
+random.randint(1,10)
+
+
+def rand_num(low, high, n):
+
+    pass
+
+for num in rand_num(1, 10, 12):
+    print(num)
+
+
+"""
+Problem 3
+Use the iter() function to convert the string below into an iterator:
+"""
+s = 'hello'
+
+#code here
+
+
+"""
+Extra Credit!
+Can you explain what gencomp is in the code below? (Note: We never covered 
+this in lecture! You will have to do some Googling/Stack Overflowing!)
+"""
+my_list = [1, 2, 3, 4, 5]
+
+gencomp = (item for item in my_list if item > 3)
+
+for item in gencomp:
+    print(item)
