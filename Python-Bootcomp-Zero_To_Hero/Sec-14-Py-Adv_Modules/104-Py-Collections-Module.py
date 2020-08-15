a12b5d54e59e82ebe0fe5fc96f8aceb028fb02b5
cameronww7 <cameronww7@gmail.com>
2020-08-15 10:21:26 -0700

    Starting Adv Modules Chapter

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/104-Py-Collections-Module.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+"""
+ Prompt 104-Py-Collections-Module
+"""
+
+print("104-Py-Collections-Module")
+
+"""
+
+Collections Module
+The collections module is a built-in module that implements specialized 
+container data types providing alternatives to Python’s general purpose 
+built-in containers. We've already gone over the basics: dict, list, set, 
+and tuple.
+
+Now we'll learn about the alternatives that the collections module provides.
+
+Counter
+Counter is a dict subclass which helps count hashable objects. Inside of 
+it elements are stored as dictionary keys and the counts of the objects 
+are stored as the value.
+
+Let's see how it can be used:
+"""
+
+print("\n104-Py-Collections-Module")
