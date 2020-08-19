832b4280d29e7abf2c9fa523700851c0d90c1225
cameronww7 <cameronww7@gmail.com>
2020-08-18 20:05:35 -0700

    Starting 105

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/105-Py-Open_and_read_Files_and_folders.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import os
+
+"""
+ Prompt 105-Py-Open_and_read_Files_and_folders
+"""
+
+print("105-Py-Open_and_read_Files_and_folders")
+
+"""
+
+"""
+
+
+
+
