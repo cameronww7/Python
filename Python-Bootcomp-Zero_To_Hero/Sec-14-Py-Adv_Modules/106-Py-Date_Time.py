31a8cad0aeb616dfad57eb2bd3a8997e02d6273a
cameronww7 <cameronww7@gmail.com>
2020-08-19 20:49:56 -0700

    Starting Date Time Module

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/106-Py-Date_Time.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+
+"""
+ Prompt 105-Py-Open_and_read_Files_and_folders
+"""
+
+print("105-Py-Open_and_read_Files_and_folders")
+
+"""
+
+datetime module
+Python has the datetime module to help deal with timestamps in 
+your code. Time values are represented with the time class. 
+Times have attributes for hour, minute, second, and microsecond. 
+They can also include time zone information. The arguments to 
+initialize a time instance are optional, but the default of 0 
+is unlikely to be what you want.
+
+time
+Let's take a look at how we can extract time information from 
+the datetime module. We can create a timestamp by specifying 
+datetime.time(hour,minute,second,microsecond)
+"""
+
