14d250df20696fee0077a11225ba8fc5378d17ad
cameronww7 <cameronww7@gmail.com>
2020-08-20 18:19:48 -0700

    Basics of Math

--- Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/107-Py-Math_and_Random.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/107-Py-Math_and_Random.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-
+import math
+import random
 
 """
  Prompt 107-Py-Math_and_Random
@@ -8,5 +9,33 @@ from __future__ import print_function
 print("107-Py-Math_and_Random")
 
 """
+Math and Random Modules
+Python comes with a built in math module and random module. In this lecture we will 
+give a brief tour of their capabilities. Usually you can simply look up the function 
+call you are looking for in the online documentation.
 
+Math Module
+https://docs.python.org/3/library/math.html
+
+Random Module
+https://docs.python.org/3/library/random.html
+
+We won't go through every function available in these modules since there are so many, 
+but we will show some useful ones.
 """
+print("\nhelp(math)")
+print("- - - - - - - - - - ")
+print(help(math))
+
+
+print("\nPlaying with Math")
+print("- - - - - - - - - - ")
+
+value = 5.4322432
+
+print(math.floor(value))
+
+print(math.ceil(value))
+
+print(round(value))
+
