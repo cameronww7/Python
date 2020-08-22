4c7ae7bdf2838ce8487bdebbf85b9a74c88d79d7
cameronww7 <cameronww7@gmail.com>
2020-08-22 09:38:41 -0700

    Regex Time!

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/109-Py-Regex.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+"""
+ Prompt 109-Py-Regex
+"""
+
+print("109-Py-Regex")
+
+"""
+Overview of Regular Expressions
+Regular Expressions (sometimes called regex for short) allows a 
+user to search for strings using almost any sort of rule they 
+can come up. For example, finding all capital letters in a string, 
+or finding a phone number in a document.
+
+Regular expressions are notorious for their seemingly strange 
+syntax. This strange syntax is a byproduct of their flexibility. 
+Regular expressions have to be able to filter out any string 
+pattern you can imagine, which is why they have a complex string 
+pattern format.
+
+Let's begin by explaining how to search for basic patterns in a string!
+
+Searching for Basic Patterns
+Let's imagine that we have the following string:
+"""
+print("\n109-Py-Regex")
+print("- - - - - - - - - - ")
