a93367ba30f0f2efa050895c6d5fc3d045745d19
cameronww7 <cameronww7@gmail.com>
2020-08-22 09:47:03 -0700

    Regex Part 1

--- Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/109-Py-Regex.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/109-Py-Regex.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import re
 
 """
  Prompt 109-Py-Regex
@@ -26,3 +27,52 @@ Let's imagine that we have the following string:
 """
 print("\n109-Py-Regex")
 print("- - - - - - - - - - ")
+
+text = "The person's phone number is 408-555-1234. Call soon!"
+
+print('phone' in text)
+
+
+print("\nphone")
+print("- - - - - - - - - - ")
+pattern = 'phone'
+print(re.search(pattern, text))
+
+
+print("\nNOT IN TEXT")
+print("- - - - - - - - - - ")
+pattern = "NOT IN TEXT"
+print(re.search(pattern, text))
+
+
+print("\nphone")
+print("- - - - - - - - - - ")
+pattern = 'phone'
+match = re.search(pattern, text)
+print(match)
+
+print("\nmatch.span()")
+print("- - - - - - - - - - ")
+print(match.span())
+
+print("\nmatch.start()")
+print("- - - - - - - - - - ")
+print(match.start())
+
+print(match.end())
+
+text = "my phone is a new phone"
+
+match = re.search("phone",text)
+
+print(match.span())
+
+
+matches = re.findall("phone",text)
+
+print(matches)
+
+for match in re.finditer("phone",text):
+    print(match.span())
+
+print(match.group())
