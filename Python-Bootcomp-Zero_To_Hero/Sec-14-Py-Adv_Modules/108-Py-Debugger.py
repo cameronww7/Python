c8f895f4b4c0abe20e20616c7c6a1506f5184d16
cameronww7 <cameronww7@gmail.com>
2020-08-21 19:10:24 -0700

    Finished Debugger

--- Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/108-Py-Debugger.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-14-Py-Adv_Modules/108-Py-Debugger.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import pdb
 
 """
  Prompt 108-Py-Debugger
@@ -23,5 +24,29 @@ the power of the pdb module.
 """
 print("\n108-Py-Debugger")
 print("- - - - - - - - - - ")
+x = [1, 3, 4]
+y = 2
+z = 3
 
+result2 = 0
+result = y + z
+print(result)
+#result2 = y+x
+print(result2)
+
+print("\nuse Debugger")
+print("- - - - - - - - - - ")
+
+x = [1, 3, 4]
+y = 2
+z = 3
+
+result = y + z
+print(result)
+
+# Set a trace using Python Debugger
+pdb.set_trace()
+
+result2 = y+x
+print(result2)
 
