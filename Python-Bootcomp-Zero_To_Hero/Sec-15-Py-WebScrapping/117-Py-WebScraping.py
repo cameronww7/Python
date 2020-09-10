42a6979a804bd2f3ba104e5a11ec1661e0fd2e42
cameronww7 <cameronww7@gmail.com>
2020-09-09 20:16:39 -0700

    Scrapped webiste for 2 star books and dispalyed them

--- Python-Bootcomp-Zero_To_Hero/Sec-15-Py-WebScrapping/117-Py-WebScraping.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-15-Py-WebScrapping/117-Py-WebScraping.py
@@ -131,4 +131,38 @@ res = requests.get(base_url.format('1'))
 
 soup = bs4.BeautifulSoup(res.text, "lxml")
 
-print(soup.select(".product_pod"))
+#print(soup.select(".product_pod"))
+
+products = soup.select(".product_pod")
+
+example = products[0]
+
+print(type(example))
+
+
+print(list(example.children))
+
+print(example.attrs)
+
+print(example.select('.star-rating.Three'))
+
+print(example.select('.star-rating.Two'))
+
+two_star_titles = []
+
+for n in range(1, 51):
+
+    scrape_url = base_url.format(n)
+    res = requests.get(scrape_url)
+
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    books = soup.select(".product_pod")
+
+    for book in books:
+        if len(book.select('.star-rating.Two')) != 0:
+            two_star_titles.append(book.select('a')[1]['title'])
+
+
+for index in two_star_titles:
+    print(index)
+
