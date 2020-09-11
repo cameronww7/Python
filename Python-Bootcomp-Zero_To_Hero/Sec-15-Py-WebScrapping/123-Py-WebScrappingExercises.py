8fd21e98af074a45296a1572bb9ebac407b217b7
cameronww7 <cameronww7@gmail.com>
2020-09-10 20:07:39 -0700

    Scrapped Website

--- Python-Bootcomp-Zero_To_Hero/Sec-15-Py-WebScrapping/123-Py-WebScrappingExercises.py
+++ Python-Bootcomp-Zero_To_Hero/Sec-15-Py-WebScrapping/123-Py-WebScrappingExercises.py
@@ -15,3 +15,11 @@ print("123-Py-WebScrappingExercises")
 print("\n123-Py-WebScrappingExercises")
 print("- - - - - - - - - - ")
 
+
+# TASK: Use requests library and BeautifulSoup to connect to
+# http://quotes.toscrape.com/ and get the HMTL text from the homepage.
+
+res = requests.get("http://quotes.toscrape.com/")
+
+print(res)
+print(res.text)
