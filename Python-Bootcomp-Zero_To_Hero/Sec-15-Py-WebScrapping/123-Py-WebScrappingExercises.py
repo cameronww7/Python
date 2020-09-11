d9c43b06c099634d573f69bb7ac23bc91f97e4fe
cameronww7 <cameronww7@gmail.com>
2020-09-10 20:06:48 -0700

    Starting Web Scraping Exercises

---
+++ Python-Bootcomp-Zero_To_Hero/Sec-15-Py-WebScrapping/123-Py-WebScrappingExercises.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import requests
+import lxml
+import bs4
+
+"""
+ 123-Py-WebScrappingExercises
+"""
+
+print("123-Py-WebScrappingExercises")
+
+"""
+
+"""
+print("\n123-Py-WebScrappingExercises")
+print("- - - - - - - - - - ")
+
