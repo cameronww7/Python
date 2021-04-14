67d49ca64a3e8e06d2359f076fbd57ffd86694c9
cameronww7 <cameronww7@gmail.com>
2021-04-13 17:51:43 -0700

    This is a demo for boost

---
+++ CTCI/Demo.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+"""
+THis is a test demoooooo
+
+this is sweet demmo
+
+"""
+
+print("DEMO")
+
+thisVar = 0
+
+if thisVar == 0:
+    print("this is a demo")
+else:
+    print("This is no a demo")
+
+
